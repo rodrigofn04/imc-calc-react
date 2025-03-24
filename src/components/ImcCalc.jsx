8f4d42f3267391198b867d53266856a636185944
Rodrigo <141188766+rodrigofn04@users.noreply.github.com>
2025-03-24 18:43:41 -0300

    React files fixed

---
+++ src/components/ImcCalc.jsx
@@ -0,0 +1,68 @@
+import './ImcCalc.css'
+import { use, useState } from 'react'
+import Button from './Button.jsx'
+import React from 'react'
+
+const ImcCalc = ({calcImc}) => {
+    const [height, setHeight] = useState("")
+    const [weight, setWeight] = useState("")
+    
+    const clearForm = (e) =>{
+        e.preventDefault()
+
+        setWeight("")
+        setHeight("")
+    }
+
+    const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, "")
+    }
+
+    const handleHeightChange = (e) =>{
+        const updatedValue = validDigits(e.target.value)
+        setHeight(updatedValue)
+    }
+
+    const handleWeightChange = (e) =>{
+        const updatedValue = validDigits(e.target.value)
+        setWeight(updatedValue)
+    }
+
+  return (
+    <div>
+        <div id='calc-container'>
+            <h2>Calculadora de IMC</h2>
+            <form id="imc-form">
+                <div className="form-inputs">
+                    <div className="form-control">
+                        <label htmlFor="height">Altura:</label>
+                        <input 
+                        type="text" 
+                        name='height' 
+                        id='height' 
+                        placeholder='Exemplo 1,77'
+                        onChange={(e) => handleHeightChange(e)}
+                        value={height}/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="weight">Peso:</label>
+                        <input 
+                        type="text" 
+                        name='weight' 
+                        id='weight' 
+                        placeholder='Exemplo 66,3'
+                        onChange={(e) => handleWeightChange(e)} 
+                        value={weight}/>
+                    </div>
+                </div>
+                <div className="action-control">
+                    <Button id="calc-btn" text = "Calcular" action={(e) => calcImc(e, height, weight)}/>
+                    <Button id="clear-btn" text = "Limpar" action={(e) => clearForm(e)}/>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default ImcCalc
---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+export const data = [
+    {
+        min: 0,
+        max: 18.4,
+        classification: ">18,5",
+        info: "Abaixo do peso",
+        obesity: "0",
+        infoClass: "medium",
+    },
+    {
+        min: 18.5,
+        max: 24.9,
+        classification: "18,5 - 24,9",
+        info: "Saudável",
+        obesity: "0",
+        infoClass: "good",
+    },
+    {
+        min: 25,
+        max: 29.9,
+        classification: "25,0 - 29,9",
+        info: "Sobrepeso",
+        obesity: "0",
+        infoClass: "good",
+    },
+    {
+        min: 30,
+        max: 39.9,
+        classification: "30,0 - 35",
+        info: "Obesidade Classe I",
+        obesity: "I",
+        infoClass: "low",
+    },
+    {
+        min: 35.1,
+        max: 40,
+        classification: "35 - 40",
+        info: "Obesidade Classe II",
+        obesity: "II",
+        infoClass: "medium",
+      },
+    {
+        min: 40.1,
+        max: 99,
+        classification: ">40,0",
+        info: "Obesidade Classe III",
+        obesity: "III",
+        infoClass: "high",
+    },
+  ];
+
+
